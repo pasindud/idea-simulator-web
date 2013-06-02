6e7dd0479855e71d2a22962ebc15acc4c4f9dd8c
Pasindu De Silva <ghosteyes17@ymail.com>
2013-06-02 18:29:55 +0530

    env post

--- app.js
+++ app.js
@@ -88,6 +88,7 @@ app.post('/sms', function(req,res){
   }
 });
 
+var port = process.env.PORT || 5000;
 
-app.listen(3000);
+app.listen(port);
 console.log("Express server listening on port %d in mode", app.address().port);
